80fed907f3d1bf73a2f2f4641b904fa639fdc5a9
TranDucLongDUT <longmup@gmail.com>
2018-01-28 22:10:23 +0700

    Thiet Ke GUI cho Log-in done

--- app/src/main/java/com/example/vankien/dating/DangNhapActivity.java
+++ app/src/main/java/com/example/vankien/dating/DangNhapActivity.java
@@ -2,13 +2,12 @@ package com.example.vankien.dating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class DangNhapActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_dang_nhap);
     }
 }
