6878d5003d8f24154282c79244b118f0c1cfcca0
LuuNgocLan <luungoclan125690@gmail.com>
2018-03-15 16:55:32 +0700

    Add initControls, addEvents, loadData to get data from model init profile

--- app/src/main/java/com/example/vankien/dating/Views/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/vankien/dating/Views/Fragment/ProfileFragment.java
@@ -9,22 +9,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.vankien.dating.Controllers.ProfileController;
+import com.example.vankien.dating.Models.Profile;
 import com.example.vankien.dating.R;
 import com.example.vankien.dating.Views.Activity.EditProfileActivity;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
+    private TextView tvName,tvAge,tvDescription;
+    private ImageButton imgBtnEdit,imgBtnSetting;
+    private ImageView imgAvatar;
+    Profile profile;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile,container,false);
-        ImageButton imgBtnEdit = (ImageButton) view.findViewById(R.id.imgBtnEdit);
-        ImageButton imgBtnSetting = (ImageButton) view.findViewById(R.id.imgBtnSetting);
-        imgBtnEdit.setOnClickListener(this);
-        imgBtnSetting.setOnClickListener(this);
+        initControls(view);
+        loadData();
+        addEvents();
         return view;
     }
 
+
+    public void loadData(){
+        tvName.setText(profile.getmName());
+        tvAge.setText(String.valueOf(profile.getmAge()));
+        tvDescription.setText(profile.getmDescription());
+    }
+    public void initControls(View view){
+        imgBtnEdit = (ImageButton) view.findViewById(R.id.imgBtnEdit);
+        imgBtnSetting = (ImageButton) view.findViewById(R.id.imgBtnSetting);
+        imgAvatar = (ImageView) view.findViewById(R.id.imgAvatar);
+        tvName = (TextView) view.findViewById(R.id.tvName);
+        tvAge = (TextView) view.findViewById(R.id.tvAge);
+        tvDescription = (TextView) view.findViewById(R.id.tvDescription);
+        profile = ProfileController.getsInstance().getProfile();
+    }
+
+    public void addEvents(){
+        imgBtnEdit.setOnClickListener(this);
+        imgBtnSetting.setOnClickListener(this);
+    }
     @Override
     public void onClick(View view) {
         switch (view.getId()){
