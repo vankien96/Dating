7d1bd3d01a984fa7b087bd67e306686374432116
LuuNgocLan <luungoclan125690@gmail.com>
2018-05-04 14:47:37 +0700

    fix UI and add UI notification setting

---
+++ app/src/main/java/com/example/vankien/dating/views/activity/NotificationsActivity.java
@@ -0,0 +1,15 @@
+package com.example.vankien.dating.views.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vankien.dating.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
